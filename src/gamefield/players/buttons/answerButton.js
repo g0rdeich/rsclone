337978474400d9c98ccdadf60c7f9c95d8b32a6b
Aleksandr Gordeev <gordeeffff@gmail.com>
2021-01-31 13:44:42 +0300

    feat: add working answer button, no total amount of points

--- src/gamefield/players/buttons/answerButton.js
+++ src/gamefield/players/buttons/answerButton.js
@@ -1,7 +1,19 @@
 import React from'react';
+import show from "../../../functions/show";
+import changeHostText from "../../../functions/changeHostText";
+
+function showSubmitArea() {
+    const textArea = document.querySelector('.answer-text');
+    const submitBtn = document.querySelector('.submit-button');
+    show(textArea);
+    show(submitBtn);
+    changeHostText('Ваш ответ?');
+}
 
 function AnswerButton() {
-    return (<button className="button answer-button">Ответить</button>);
+    return (<button className="button answer-button"
+                    onClick={() => showSubmitArea()}>
+        Ответить</button>);
 }
 
 export default AnswerButton;
