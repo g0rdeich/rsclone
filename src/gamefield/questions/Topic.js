376ccf17d2e7cd7a975e18920679508a69172b1b
Victor Avdeev <avdeevvictor95@gmail.com>
2021-02-01 17:55:19 +0300

    Fix: add topicName property to question in Question component

--- src/gamefield/questions/Topic.js
+++ src/gamefield/questions/Topic.js
@@ -6,6 +6,7 @@ function Topic({topic}) {
         <div className="topic">
             <div className="topic-name">{topic.topicName}</div>
             {topic.topicQuestions.map((question) => {
+								question.topicName = topic.topicName;
                 return < Question question={question} key={`${topic.topicName}${question.price}`}/>
             })}
         </div>
