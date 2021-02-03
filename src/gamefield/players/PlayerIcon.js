8df67ca64799374ad929fd85b4070247e5f9b8a0
Victor Avdeev <avdeevvictor95@gmail.com>
2021-02-03 04:20:27 +0300

    feat: add logged user  synchronization with PlayerIcon

--- src/gamefield/players/PlayerIcon.js
+++ src/gamefield/players/PlayerIcon.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Avatar from "../../components/Avatar";
+import Context from '../../GlobalContext'
 
 function PlayerIcon() {
+	const { isUserLoged, loggedUser } = React.useContext(Context);
+	const [userName, setUserName] = React.useState('');
+
+	React.useEffect(() => {
+		setUserName(isUserLoged ? loggedUser.login : 'Anonymous');
+	}, [isUserLoged])
+
     return (
         <div className="player-icon">
             <div className="player-points">0</div>
-            <div>Name</div>
+            <div>{userName}</div>
             < Avatar />
         </div>
 
